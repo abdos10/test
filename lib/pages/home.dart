e8e87f5f98aa6340ad2dbe325280dcd9d0698672
abdulrazzak <abdulrazzaksabbagh10@gmail.come>
2024-09-05 19:34:46 +0300

    modifing the home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+        final double screenwidth = MediaQuery.of(context).size.width;
+    final double screenheight = MediaQuery.of(context).size.height;
+    return const SafeArea(
+      child: Scaffold(
+              backgroundColor: Constants.kBlackColor,
+              body: Stack(children: [],),
+      ),
+    );
   }
 }
