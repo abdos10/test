f889e6f4e3d06200f1945efd9e37cf6b3a157d0f
abdulrazzak <abdulrazzaksabbagh10@gmail.come>
2024-09-06 18:06:16 +0300

    putting the spots in the homepage's background

--- lib/components/spotwidget.dart
+++ lib/components/spotwidget.dart
@@ -1,10 +1,35 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SpotWidget extends StatelessWidget {
-  const SpotWidget({super.key});
+   double? left;
+  double? top;
+  double? right;
+  final Color color;
+
+  SpotWidget({super.key, this.left, this.top, this.right, required this.color});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final double screenwidth = MediaQuery.of(context).size.width;
+    final double screenheight = MediaQuery.of(context).size.height;
+    return  Positioned(
+      top: top,
+      left: left,
+      right: right,
+      child: Container(
+        width: screenwidth * 0.5,
+        height: screenheight * 0.4,
+        decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 200, sigmaY: 200),
+          child: SizedBox(
+            width: screenwidth * 0.3,
+            height: screenheight * 0.2,
+          ),
+        ),
+      ),
+    );;
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/components/spotwidget.dart';
 import 'package:movieapp/constants.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,10 +9,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
         final double screenwidth = MediaQuery.of(context).size.width;
     final double screenheight = MediaQuery.of(context).size.height;
-    return const SafeArea(
+    return  SafeArea(
       child: Scaffold(
               backgroundColor: Constants.kBlackColor,
-              body: Stack(children: [],),
+              body: Stack(children: [
+                 SpotWidget(color: Constants.kGreenColor, right: -170),
+              SpotWidget(color: Constants.kPinkColor, left: -170),
+              SpotWidget(
+                color: Constants.kPinkColor,
+                left: -200,
+                top: screenwidth * 0.8,
+              ),
+              SpotWidget(
+                color: Constants.kGreenColor,
+                right: -200,
+                top: screenwidth * 0.8,
+              ),
+              ],),
       ),
     );
   }
